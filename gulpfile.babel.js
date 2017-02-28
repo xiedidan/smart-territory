b6906d85b8accb4125193166ed0bc38fa26bdbc0
xiedidan <xiedidan@yeah.net>
2017-02-28 21:55:10 +0800

    gulp engineering

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,11 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import sourcemaps from 'gulp-sourcemaps'
+import clean from 'gulp-clean'
+import sequence from 'gulp-sequence'
 
+// compiling tasks
+gulp.task('build', sequence('clean', 'compile'))
 gulp.task('compile', ['compile-express', 'compile-routes', 'compile-mobx'])
 
 gulp.task('compile-express', () => {
@@ -33,4 +37,34 @@ gulp.task('compile-mobx', () => {
     .pipe(gulp.dest('public/javascripts/mbox-dist'))
 })
 
-gulp.watch(['express-src/**/*.js', 'routes-src/**/*.js', 'mobx-src/**/*.js'], ['compile'])
+// cleaning tasks
+gulp.task('clean', ['clean-express', 'clean-routes', 'clean-mobx'])
+
+gulp.task('clean-express', () => {
+  return gulp.src('express-dist/**/*.js', {read: false})
+    .pipe(clean())
+})
+
+gulp.task('clean-routes', () => {
+  return gulp.src('routes/**/*.js', {read: false})
+    .pipe(clean())
+})
+
+gulp.task('clean-mobx', () => {
+  return gulp.src('public/javascripts/mbox-dist/**/*.js', {read: false})
+    .pipe(clean())
+})
+
+// watching tasks
+gulp.task('watch', () => {
+  let watcher = gulp.watch(['express-src/**/*.js', 'routes-src/**/*.js', 'mobx-src/**/*.js'], (event) => {
+    sequence('clean', 'compile')(err => {
+      if (err)
+        console.log('*** Watcher - Error ***', err)
+    })
+  })
+
+  watcher.on('change', event => {
+    console.log('*** Watcher - File ' + event.path + ' was ' + event.type + ', compiling... ***')
+  })
+})
